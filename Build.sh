9498f44bf0794fb781f8fbcb6ddb700e6005b13b
Alexander S. <alexander@sharapov.biz>
2025-12-23 21:13:26 +0600

    Removed outdated `ffmpeg-hls.patch` and `ffmpeg-jsonstats.patch` files.

--- Build.sh
+++ Build.sh
@@ -4,8 +4,8 @@ set -au
 
 function build_default_native() {
   export OS_NAME=alpine
-  export OS_VERSION=3.21
-  export FFMPEG_VERSION=7.1.1
+  export OS_VERSION=3.23.2
+  export FFMPEG_VERSION=8.0
 
   docker build \
     --progress=plain \
@@ -17,8 +17,8 @@ function build_default_native() {
 
 function build_default() {
   export OS_NAME=alpine
-  export OS_VERSION=3.21
-  export FFMPEG_VERSION=7.1.1
+  export OS_VERSION=3.23.2
+  export FFMPEG_VERSION=8.0
 
   docker buildx build \
     --load \
@@ -32,8 +32,8 @@ function build_default() {
 
 function build_rpi() {
   export OS_NAME=alpine
-  export OS_VERSION=3.21
-  export FFMPEG_VERSION=7.1.1
+  export OS_VERSION=3.23.2
+  export FFMPEG_VERSION=8.0
 
   docker build \
     --progress=plain \
@@ -46,8 +46,9 @@ function build_rpi() {
 function build_cuda12() {
   export OS_NAME=ubuntu
   export OS_VERSION=24.04
-  export FFMPEG_VERSION=7.1.1
+  export FFMPEG_VERSION=8.0
   export FFNVCODEC_VERSION=12.2.72.0
+  export CUDA_VERSION=12.9.1
 
   docker build \
     --progress=plain \
@@ -59,10 +60,27 @@ function build_cuda12() {
     -t sharapov/flumixa-base:ffmpeg${FFMPEG_VERSION}-cuda-ubuntu$OS_VERSION-cuda${CUDA_VERSION} .
 }
 
+function build_cuda13() {
+  export OS_NAME=ubuntu
+  export OS_VERSION=24.04
+  export FFMPEG_VERSION=8.0
+  export FFNVCODEC_VERSION=12.2.72.0
+  export CUDA_VERSION=13.1.0
+
+  docker build \
+    --progress=plain \
+    --build-arg BUILD_IMAGE=nvidia/cuda:$CUDA_VERSION-devel-ubuntu$OS_VERSION \
+    --build-arg DEPLOY_IMAGE=nvidia/cuda:$CUDA_VERSION-runtime-ubuntu$OS_VERSION \
+    --build-arg FFNVCODEC_VERSION=$FFNVCODEC_VERSION \
+    --build-arg FFMPEG_VERSION=$FFMPEG_VERSION \
+    -f Dockerfile.ubuntu.cuda13 \
+    -t sharapov/flumixa-base:ffmpeg${FFMPEG_VERSION}-cuda-ubuntu$OS_VERSION-cuda${CUDA_VERSION} .
+}
+
 function build_vaapi() {
   export OS_NAME=ubuntu
   export OS_VERSION=24.04
-  export FFMPEG_VERSION=7.1.1
+  export FFMPEG_VERSION=8.0
 
   docker buildx build \
     --load \
@@ -77,7 +95,7 @@ function build_vaapi() {
 
 main() {
   if [[ $# == 0 ]]; then
-    echo "Options available: default, default_native, rpi, cuda12, vaapi"
+    echo "Options available: default, default_native, rpi, cuda12, cuda13, vaapi"
     exit 0
   else
     if [[ $1 == "default" ]]; then
@@ -88,6 +106,8 @@ main() {
       build_rpi
     elif [[ $1 == "cuda12" ]]; then
       build_cuda12
+    elif [[ $1 == "cuda13" ]]; then
+      build_cuda13
     elif [[ $1 == "vaapi" ]]; then
       build_vaapi
     fi
